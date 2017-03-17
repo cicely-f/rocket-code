843f2775ffe18b8cb76596be8c687f15bf0ccba3
André Fischer <andre.fischer@chas.se>
2017-03-17 13:49:57 +0100

    get Rocket connection info from environment

--- src/extension.ts
+++ src/extension.ts
@@ -9,12 +9,13 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "rocket-code" is now active!');
+    const { ROCKET_SERVER, ROCKET_USER, ROCKET_PASSWORD } = process.env;
+    console.log(`Starting up Rocket.Code on '${ROCKET_SERVER}' for user '${ROCKET_USER}'`);
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    const disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
